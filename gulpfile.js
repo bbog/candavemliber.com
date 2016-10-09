6e88f23c7330d6f2ccbdf31855c8f6ee73e1c14b
Bogdan Bucur <contact@bogdanbucur.eu>
2016-10-09 20:35:45 +0300

    Added gulp script
    Added a gulp script for the minification/uglification of the CSS and JS
    files.
    It can be run with `gulp build` or just `gulp` or with `gulp build-css`
    or `gulp build-js` for just the CSS or JS part.

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+
+
+
+var gulp     = require('gulp'),
+	cleanCSS = require('gulp-clean-css'),
+	uglifyJS = require('gulp-uglify'),
+	rename   = require('gulp-rename'),
+	concat   = require('gulp-concat');
+
+
+gulp.task('build-css', function() {
+	return gulp.src('css/style.css')
+		.pipe(cleanCSS({ compatibility: 'ie8' }))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest('css/'))
+});
+
+
+var js_files = ['js/moment.min.js', 'js/main.js'];
+gulp.task('build-js', function() {
+	return gulp.src(js_files)
+		.pipe(concat('main.min.js'))
+		.pipe(gulp.dest('js'))
+		.pipe(uglifyJS({ preserveComments: 'license' }))
+		.pipe(gulp.dest('js'))
+});
+
+gulp.task('build', ['build-css', 'build-js']);
+
+gulp.task('default', ['build']);
